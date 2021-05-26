0139aad23dd1edd55433ca406da778b5703234b1
Lin <13871876+tianlinxu312@users.noreply.github.com>
2021-05-26 11:15:51 +0100

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -32,9 +32,8 @@ def load_dataset(dataset):
         from data.kth import KTH
         train_data = KTH(train=True)
     elif dataset == 'mazes':
-        from data.mazes import DataReader
-        data_reader = DataReader(dataset=dataset, time_steps=48)
-        train_data = data_reader.provide_dataset()
+        from data.mazes import Mazes
+        data_reader = Mazes()
     return train_data
 
 
